009b757b44d4ee187ed7e11981bbd9b609278e9e
justsicklife <justsicklife@gmail.com>
2021-01-30 22:45:39 +0900

    페이지 이동 버튼,최적화,구현

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import BoxOfficeContainer from "./container/BoxOfficeContainer.js";
 import MovieDetailContainer from "./container/MovieDetailContainer.js";
 import NavbarContainer from "./container/NavbarContainer.js";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import MovieListContainer from "./container/MovieListContainer.js";
 import CompanyContainer from "./container/CompanyContainer.js";
 import CompanyDetailContainer from "./container/CompanyDetailContainer.js";
@@ -12,15 +12,20 @@ import PeopleDetailContainer from './container/PeopleDetailContainer';
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Route path="/" component={NavbarContainer} />
-        <Route exact path="/" component={BoxOfficeContainer} />
-        <Route path="/movielist" component={MovieListContainer} />
-        <Route path="/companys" component={CompanyContainer} />
-        <Route path="/company/detail/:companyid" component={CompanyDetailContainer} />
-        <Route path="/peoplelist" component={PeopleContainer} />
-        <Route path="/people/detail/:id" component={PeopleDetailContainer} />
-        <Route path="/movie/detail/:id" component={MovieDetailContainer} />
+        <Switch>
+          <Route exact path="/" component={BoxOfficeContainer} />
+          <Route path="/movielist" component={MovieListContainer} />
+          <Route path="/companys" component={CompanyContainer} />
+          <Route path="/company/detail/:companyid" component={CompanyDetailContainer} />
+          <Route path="/peoplelist" component={PeopleContainer} />
+          <Route path="/people/detail/:id" component={PeopleDetailContainer} />
+          <Route path="/movie/detail/:id" component={MovieDetailContainer} />
+          <Route path="*" component={() => {
+            return <div className="notfound_page"><h5>404 Not Found</h5></div>
+          }} />
+        </Switch>
       </Router>
     </div>
   );
