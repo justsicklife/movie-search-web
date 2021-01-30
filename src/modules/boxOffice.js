6b156be41b49c0b948a27ae6f79740bfa260d5e9
justsicklife <justsicklife@gmail.com>
2021-01-30 22:56:32 +0900

    데이터 받아오는 주소를 http 에서 https로 테스트 겸 변경

--- src/modules/boxOffice.js
+++ src/modules/boxOffice.js
@@ -8,7 +8,7 @@ export const getBoxOffice = () => async (dispatch, getState) => {
     dispatch({ type: GET_BOXOFFICE });
     try {
         const { date } = getState().boxOffice;
-        const res = await fetch(`http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=7c88cc83cd33def078fa2c0580e6045c&targetDt=${date}`);
+        const res = await fetch(`https://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=7c88cc83cd33def078fa2c0580e6045c&targetDt=${date}`);
         const resJson = await res.json();
         const data = resJson.boxOfficeResult.dailyBoxOfficeList;
         dispatch({ type: GET_BOXOFFICE_SUCCESS, data });
